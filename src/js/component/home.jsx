01bf45ff5d05654adb7434a9c94d3e11c6271dc0
Cristian Chacon <cristianchacohn@gmail.com>
2024-12-11 14:07:10 +0000

    ter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("")
+	console.log(inputValue);
+
+	const [taskList, setTaskList] = useState([])
+	 
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<header className="header">
+				<h1>todos</h1>
+				<form >
+					<input
+						autoFocus={true}
+						className="new-todo"
+						placeholder="What needs to be done?"
+						onChange={(e) => setInputValue(e.target.value)}
+						onKeyDown={(e)=>{console.log(e.key);
+						}}
+					/>
+				</form>
+			</header>
+			<ul className="todo-list">
+
+			</ul>
 		</div>
 	);
 };
